181228c5c4f2a070626c7115dfc53c91dc964f58
Joaquin <clcflecha@gmail.com>
2024-12-04 09:56:51 -0300

    inicio

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,15 +1,28 @@
 import { src, dest, watch, series } from 'gulp';
-
 import * as sass from 'sass';
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
-
-import imagemin from 'gulp-imagemin';
 import webp from 'gulp-webp';
 
 const sassCompiler = gulpSass(sass);
 
+// Definir las rutas
+const paths = {
+    dist: {
+        img: {
+            dir: './build/img', // Carpeta de destino para las imágenes
+        },
+    },
+    src: {
+        img: {
+            dir: './src/img', // Carpeta de origen de las imágenes
+            files: './src/img/**/*', // Todos los archivos de imagen
+        },
+    },
+};
+
+// Tarea CSS
 function css(done) {
     src('src/scss/app.scss')
         .pipe(sassCompiler().on('error', sassCompiler.logError))
@@ -18,21 +31,26 @@ function css(done) {
     done();
 }
 
-function imagenes() {
-    return src('src/img/**/*')
-        .pipe(imagemin({ optimizationLevel: 3 }))
-        .pipe(dest('build/img'));
+// Tarea para copiar las imágenes sin procesarlas
+function imagenes(done) {
+    src(paths.src.img.files, { encoding: false })  // Usamos encoding: false para evitar problemas con archivos binarios
+        .pipe(dest(paths.dist.img.dir));  // Copiamos las imágenes a la carpeta build/img
+    done();
 }
 
-function versionWebp() {
-    return src('src/img/**/*.{png,jpg}')
-        .pipe(webp())
-        .pipe(dest('build/img'));
+// Tarea para convertir imágenes a WebP (opcional)
+function versionWebp(done) {
+    src('src/img/**/*.{png,jpg}')  // Solo imágenes PNG y JPG
+        .pipe(webp())  // Convertir a WebP
+        .pipe(dest('build/img'));  // Guardarlas en build/img
+    done();
 }
 
+// Tarea para observar cambios
 function dev() {
     watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
+    watch('src/img/**/*', series(imagenes, versionWebp));  // Copiar imágenes y luego convertir a WebP si es necesario
 }
 
+// Tarea por defecto
 export default series(imagenes, versionWebp, css, dev);
